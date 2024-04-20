0d3cd9d237cb999761fff527d9f864f174a57179
Ersapta Aristo <aristoersapta@gmail.com>
2024-04-20 18:55:48 +0700

    include svg import along with the config

---
+++ src/components/Icons/index.ts
@@ -0,0 +1,51 @@
+import IconArrowRight from "@/../public/images/icon-arrow-right.svg";
+import IconChangesSaved from "@/../public/images/icon-changes-saved.svg";
+import IconChevronDown from "@/../public/images/icon-chevron-down.svg";
+import IconCodeWars from "@/../public/images/icon-codewars.svg";
+import IconDevto from "@/../public/images/icon-devto.svg";
+import IconDragAndDrop from "@/../public/images/icon-drag-and-drop.svg";
+import IconEmail from "@/../public/images/icon-email.svg";
+import IconFacebook from "@/../public/images/icon-facebook.svg";
+import IconFreecodecamp from "@/../public/images/icon-freecodecamp.svg";
+import IconFrontendMentor from "@/../public/images/icon-frontend-mentor.svg";
+import IconGithub from "@/../public/images/icon-github.svg";
+import IconGitlab from "@/../public/images/icon-gitlab.svg";
+import IconHashnode from "@/../public/images/icon-hashnode.svg";
+import IconLinkCopiedToClipboard from "@/../public/images/icon-link-copied-to-clipboard.svg";
+import IconLink from "@/../public/images/icon-link.svg";
+import IconLinkedin from "@/../public/images/icon-linkedin.svg";
+import IconPassword from "@/../public/images/icon-password.svg";
+import IconPreviewHeader from "@/../public/images/icon-preview-header.svg";
+import IconProfileDetailsHeader from "@/../public/images/icon-profile-details-header.svg";
+import IconStackOverflow from "@/../public/images/icon-stack-overflow.svg";
+import IconTwitch from "@/../public/images/icon-twitch.svg";
+import IconTwitter from "@/../public/images/icon-twitter.svg";
+import IconUploadImage from "@/../public/images/icon-upload-image.svg";
+import IconYoutube from "@/../public/images/icon-youtube.svg";
+
+export {
+  IconArrowRight,
+  IconChangesSaved,
+  IconChevronDown,
+  IconCodeWars,
+  IconDevto,
+  IconDragAndDrop,
+  IconEmail,
+  IconFacebook,
+  IconFreecodecamp,
+  IconFrontendMentor,
+  IconGithub,
+  IconGitlab,
+  IconHashnode,
+  IconLinkCopiedToClipboard,
+  IconLink,
+  IconLinkedin,
+  IconPassword,
+  IconPreviewHeader,
+  IconProfileDetailsHeader,
+  IconStackOverflow,
+  IconTwitch,
+  IconTwitter,
+  IconUploadImage,
+  IconYoutube,
+};
