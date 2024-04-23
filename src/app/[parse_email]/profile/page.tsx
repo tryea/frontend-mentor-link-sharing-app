75008676161141f521dd25d8d50fc5723e178045
Ersapta Aristo <aristoersapta@gmail.com>
2024-04-23 18:11:15 +0700

    add integration photo profile with clerk

--- src/app/[parse_email]/profile/page.tsx
+++ src/app/[parse_email]/profile/page.tsx
@@ -3,8 +3,8 @@
 import Button from "@/components/Button";
 import EditorContentContainer from "@/components/EditorContentContainer";
 import EditorNavbar from "@/components/EditorNavbar";
-import { IconUploadImage } from "@/components/Icons";
 import ProfileInput from "@/components/ProfileInput";
+import UploadPhotoProfile from "@/components/UploadPhotoProfile";
 import { useUser } from "@clerk/nextjs";
 import { useGSAP } from "@gsap/react";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -18,17 +18,20 @@ export type EditUserProfileFormData = {
   first_name: string;
   last_name: string;
   email: string;
+  photo_profile: FileList | string | null;
 };
 export default function EditUserProfilePage() {
   const { isLoaded, user } = useUser();
   const overlayRef = useRef<HTMLDivElement | null>(null);
 
   const [loading, setLoading] = useState(false);
+  const [file, setFile] = useState<File | string | null>(null);
 
   const EditUserProfileFormSchema = z.object({
     first_name: z.string().min(1, "can't be empty"),
     last_name: z.string().min(1, "can't be empty"),
     email: z.string().min(1).email(),
+    photo_profile: z.any(),
   });
 
   const {
@@ -42,11 +45,13 @@ export default function EditUserProfilePage() {
 
   useEffect(() => {
     if (isLoaded) {
-      const { firstName, lastName, primaryEmailAddress } = user!;
+      const { firstName, lastName, primaryEmailAddress, imageUrl } = user!;
 
       setValue("first_name", firstName || "");
       setValue("last_name", lastName || "");
       setValue("email", primaryEmailAddress!.emailAddress);
+      setValue("photo_profile", imageUrl);
+      setFile(imageUrl);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isLoaded]);
@@ -55,12 +60,19 @@ export default function EditUserProfilePage() {
     console.log(data);
     try {
       setLoading(true);
+
+      if (data.photo_profile instanceof FileList) {
+        await user!.setProfileImage({
+          file: data.photo_profile[0],
+        });
+      }
+
       await user!.update({
         firstName: data.first_name,
         lastName: data.last_name,
       });
-    } catch (err) {
-      console.log(err);
+    } catch (err: any) {
+      console.log(JSON.stringify(err, null, 2));
     } finally {
       setLoading(false);
     }
@@ -110,7 +122,11 @@ export default function EditUserProfilePage() {
     <main className="flex flex-col flex-1 h-svh max-h-svh bg-light_grey">
       <EditorNavbar />
       <EditorContentContainer>
-        <div className="flex flex-1 bg-white flex-col gap-10 p-6 max-h-full overscroll-contain overflow-y-scroll scrollbar-none">
+        <form
+          id="editUserProfileForm"
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex flex-1 bg-white flex-col gap-10 p-6 max-h-full overscroll-contain overflow-y-scroll scrollbar-none"
+        >
           <div className="flex flex-col gap-2">
             <h2 className="heading-base text-dark_grey">
               Customize your links
@@ -123,23 +139,18 @@ export default function EditUserProfilePage() {
 
           <div className="p-5 flex flex-col gap-3 bg-light_grey rounded-[12px]">
             <label className="body-m text-grey">Profile picture</label>
-            <div className="w-full flex flex-col gap-6">
-              <div className="w-[192px] h-[192px] bg-light_purple flex items-center justify-center gap-2 flex-col rounded-[12px]">
-                <IconUploadImage />
-                <p className="heading-s text-purple">+ Upload Image</p>
-                <input type="file" className="hidden" />
-              </div>
-              <p className="body-s text-grey">
-                Image must be below 1024x1024px. Use PNG or JPG format.
-              </p>
-            </div>
+            <UploadPhotoProfile
+              name="photo_profile"
+              error={errors.photo_profile}
+              register={register}
+              value={file}
+              onChange={(file) => {
+                setFile(file);
+              }}
+            />
           </div>
 
-          <form
-            id="editUserProfileForm"
-            onSubmit={handleSubmit(onSubmit)}
-            className="p-5 flex flex-col gap-3 bg-light_grey rounded-[12px]"
-          >
+          <div className="p-5 flex flex-col gap-3 bg-light_grey rounded-[12px]">
             <ProfileInput
               register={register}
               error={errors.first_name}
@@ -164,8 +175,8 @@ export default function EditUserProfilePage() {
               placeholder="Email"
               name="email"
             />
-          </form>
-        </div>
+          </div>
+        </form>
         <div className="flex flex-col w-full">
           <hr className="h-px bg-borders w-full" />
           <div className="p-4">
---
+++ src/components/UploadPhotoProfile/index.tsx
@@ -0,0 +1,86 @@
+import { FieldError, UseFormRegister } from "react-hook-form";
+import { IconUploadImage } from "../Icons";
+import { MouseEvent, useEffect, useRef, useState } from "react";
+import Image from "next/image";
+
+export type TUploadPhotoProfileProps = {
+  name: string;
+  error: FieldError | undefined;
+  register: UseFormRegister<any>;
+  value: File | string | null;
+  onChange: (file: File | null) => void;
+};
+export default function UploadPhotoProfile(props: TUploadPhotoProfileProps) {
+  console.log(typeof props.value);
+  console.log(props.value);
+
+  const inputFileRef = useRef<HTMLInputElement | null>(null);
+  const [loading, setLoading] = useState(false);
+  const { ref, onChange, ...rest } = props.register(props.name);
+
+  const onFileContainerClick = (e: MouseEvent<HTMLDivElement>) => {
+    e.preventDefault();
+    inputFileRef.current?.click();
+  };
+
+  useEffect(() => {
+    console.log({ loading });
+
+    if (typeof props.value === "string") {
+      setLoading(true);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [props.value]);
+
+  return (
+    <div className="w-full flex flex-col gap-6">
+      <div
+        onClick={onFileContainerClick}
+        className="relative w-[192px] h-[192px] bg-light_purple flex items-center justify-center gap-2 flex-col rounded-[12px]"
+      >
+        {props.value ? (
+          <>
+            {loading && <p className="body-s text-purple">Loading...</p>}
+            <Image
+              src={
+                props.value instanceof File
+                  ? URL.createObjectURL(props.value)
+                  : props.value
+              }
+              onLoad={() => setLoading(false)}
+              onLoadStart={() => setLoading(true)}
+              alt="user photo profile"
+              fill
+              className="rounded-[12px]"
+            />
+          </>
+        ) : (
+          <>
+            <IconUploadImage />
+            <p className="heading-s text-purple">+ Upload Image</p>
+          </>
+        )}
+      </div>
+      <p className="body-s text-grey">
+        Image must be below 1024x1024px. Use PNG or JPG format.
+      </p>
+      <input
+        {...rest}
+        ref={(e) => {
+          ref(e);
+          inputFileRef.current = e;
+        }}
+        onChange={(e) => {
+          const file = e.target.files ? e.target.files[0] : null;
+
+          props.onChange(file);
+          onChange(e);
+        }}
+        multiple={false}
+        accept=".jpg,.png"
+        type="file"
+        className="hidden"
+      />
+    </div>
+  );
+}
